90228aba5bd0556853e92a886a2d9a7120f9e452
bagguo <bagguo23@gmail.com>
2021-09-27 11:28:54 +0800

    recyclerview & SwipeRefreshLayout

---
+++ recyclerviewdemo/src/main/java/com/example/recyclerviewdemo/FeedArticleBean.java
@@ -0,0 +1,113 @@
+package com.example.recyclerviewdemo;
+
+class FeedArticleBean {
+    /**
+     * apkLink: "",
+     * audit: 1,
+     * author: "",
+     * canEdit: false,
+     * chapterId: 502,
+     * chapterName: "自助",
+     * collect: false,
+     * courseId: 13,
+     * desc: "",
+     * descMd: "",
+     * envelopePic: "",
+     * fresh: true,
+     * id: 16049,
+     * link: "https://mp.weixin.qq.com/s/fEgSymIZMm82T9CrLueIKA",
+     * niceDate: "2小时前",
+     * niceShareDate: "2小时前",
+     * origin: "",
+     * prefix: "",
+     * projectLink: "",
+     * publishTime: 1605143247000,
+     * realSuperChapterId: 493,
+     * selfVisible: 0,
+     * shareDate: 1605143247000,
+     * shareUser: "飞洋",
+     * superChapterId: 494,
+     * superChapterName: "广场Tab",
+     * tags:[],
+     * title: "🔥“终于懂了“系列：Jetpack AAC完整解析（-）Lifecycle 完全掌握！",
+     * type: 0,
+     * userId: 31360,
+     * visible: 1,
+     * zan: 0
+     */
+    private String apkLink;
+    private int audit;
+    //    author
+//            canEdit
+//    chapterId
+//            chapterName
+//    collect
+//            courseId
+//    desc
+//            descMd
+//    envelopePic
+//            fresh
+//    id
+    //            niceShareDate
+//    origin
+//            prefix
+//    projectLink
+//            publishTime
+//    realSuperChapterId
+//            selfVisible
+//    shareDate
+//            shareUser
+//    superChapterId
+//            superChapterName
+//    tags
+    private String title;
+    //    type
+//            userId
+//    visible
+//            zan
+    private String link;
+    private String niceDate;
+
+
+    public String getApkLink() {
+        return apkLink;
+    }
+
+    public void setApkLink(String apkLink) {
+        this.apkLink = apkLink;
+    }
+
+    public int getAudit() {
+        return audit;
+    }
+
+    public void setAudit(int audit) {
+        this.audit = audit;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getNiceDate() {
+        return niceDate;
+    }
+
+    public void setNiceDate(String niceDate) {
+        this.niceDate = niceDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
---
+++ recyclerviewdemo/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.recyclerviewdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+        SwipeRefreshLayout mSwipeRefreshLayout;
+    RecyclerView mRecyclerview;
+    MyAdapter myAdapter;
+
+    List<FeedArticleBean> mList = new ArrayList<>();
+
+    private MyHandler myHandler = new MyHandler(this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSwipeRefreshLayout = findViewById(R.id.swiperefreshlayout);
+        mSwipeRefreshLayout.setColorSchemeResources(R.color.purple_700
+                , R.color.purple_200, R.color.white, R.color.teal_200);
+
+//        设置下拉刷新时的操作
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                requestData();
+            }
+        });
+
+        mRecyclerview = findViewById(R.id.recyclerview);
+        mRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        myAdapter = new MyAdapter(this, mList);
+        mRecyclerview.setAdapter(myAdapter);
+
+        requestData();
+    }
+
+    private void requestData() {
+        final String HOST = "https://www.wanandroid.com/";
+        String url = HOST + "article/list/0/json";
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                String s = response.body().string();
+
+                try {
+                    JSONObject object = JSONObject.parseObject(s);
+                    JSONObject data = object.getJSONObject("data");
+                    String datas = data.getString("datas");
+                    List<FeedArticleBean> articls = JSON.parseArray(datas, FeedArticleBean.class);
+
+                    Message message = new Message();
+                    message.obj = articls;
+                    message.what = 200;
+                    myHandler.sendMessage(message);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+
+    }
+
+    static class MyHandler extends Handler {
+        private WeakReference<MainActivity> weakReference;
+
+        public MyHandler(MainActivity activity) {
+            this.weakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 100:
+                    Toast.makeText(weakReference.get(), "failed", Toast.LENGTH_LONG).show();
+                    break;
+                case 200:
+                    List<FeedArticleBean> list = (ArrayList<FeedArticleBean>) msg.obj;
+                    weakReference.get().mList.clear();
+                    weakReference.get().mList.addAll(list);
+                    weakReference.get().myAdapter.notifyDataSetChanged();
+                    weakReference.get().mSwipeRefreshLayout.setRefreshing(false);
+                    break;
+            }
+
+        }
+    }
+}
