edd82c5d436c484f995a4ea63eb67b4ca8ab8241
bagguo <bagguo23@gmail.com>
2022-10-12 11:33:29 +0800

    ListView demo

--- ui/src/main/java/com/example/ui/UIMainActivity.java
+++ ui/src/main/java/com/example/ui/UIMainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import com.example.ui.butterknife.ButterKnifeTestActivity;
 import com.example.ui.dialog.DialogDemoActivity;
 import com.example.ui.gridview.GridViewDemoActivity;
+import com.example.ui.listview.ListViewDemoActivity;
 import com.example.ui.multitype.normal.NormalActivity;
 import com.example.ui.tablayout.TabLayoutDemoActivity;
 import com.example.ui.toolbar.ToolbarSimpleActivity;
@@ -21,6 +22,11 @@ public class UIMainActivity extends AppCompatActivity {
 
         findViewById(R.id.toolbar_simple).setOnClickListener(view -> {
             Intent intent = new Intent(UIMainActivity.this, ToolbarSimpleActivity.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.btn_list_view).setOnClickListener(view -> {
+            Intent intent = new Intent(UIMainActivity.this, ListViewDemoActivity.class);
             startActivity(intent);
         });
 
