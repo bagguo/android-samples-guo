e4aa685f79821f9cbfcf18f4bae7de23d1eec58d
bagguo <bagguo23@gmail.com>
2022-11-01 19:52:03 +0800

    WebView

---
+++ app/src/main/java/com/example/android_lesson/webview/WebViewDemoActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_lesson.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebViewDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view_demo);
+    }
+}
