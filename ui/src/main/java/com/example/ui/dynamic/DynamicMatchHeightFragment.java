3e8c52857c6d15b40523e2aa5eb767e67a519e74
darren.eth <bagguo23@gmail.com>
2023-08-10 17:14:36 +0800

    动态设置fragment高度，解决fragment依附的view wrap，fragment不能match屏幕的情况

---
+++ ui/src/main/java/com/example/ui/dynamic/DynamicMatchHeightFragment.java
@@ -0,0 +1,24 @@
+package com.example.ui.dynamic;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ui.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class DynamicMatchHeightFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dynamic_match_height, container, false);
+        return view;
+
+    }
+}
