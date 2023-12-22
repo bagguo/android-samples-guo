0689627c97461b1cbbf75e1d7240935790888dc8
bagguo <bagguo23@gmail.com>
2023-12-22 19:07:28 +0800

    patch of RxJava samples

--- app/src/main/java/com/example/android_lesson/async/rxjava/RxjavaTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.android_lesson.async.rxjava;
-
-import io.reactivex.Flowable;
-
-public class RxjavaTest {
-    public void helloWorld() {
-        Flowable.just("Hello world").subscribe(System.out::println);
-    }
-}
