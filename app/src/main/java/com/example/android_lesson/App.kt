b6950bc1e5ed110efb1b016a1d5b5c939ddb014c
bagguo <bagguo23@gmail.com>
2024-06-03 17:18:58 +0800

    code format

--- app/src/main/java/com/example/android_lesson/App.kt
+++ app/src/main/java/com/example/android_lesson/App.kt
@@ -2,6 +2,7 @@ package com.example.android_lesson
 
 import android.app.Application
 import android.content.Context
+import com.alibaba.android.arouter.launcher.ARouter
 
 class App : Application() {
 
@@ -12,5 +13,19 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         mContext = this
+
+        initARouter()
+    }
+
+    private fun initARouter() {
+        //启动优化－开始抓取TraceView
+        //        Debug.startMethodTracing("trace_view_start");
+
+        //        if (isDebug()) {           // These two lines must be written before init, otherwise these configurations will be invalid in the init process
+        ARouter.openLog() // Print log
+        ARouter.openDebug() // Turn on debugging mode (If you are running in InstantRun mode, you must turn on debug mode! Online version needs to be closed, otherwise there is a security risk)
+
+        //        }
+        ARouter.init(this) // As early as possible, it is recommended to initialize in the Application
     }
 }
--- app/src/main/java/com/example/android_lesson/wallet/WalletActivity.java
+++ app/src/main/java/com/example/android_lesson/wallet/WalletActivity.java
@@ -8,8 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.android_lesson.MyApplication;
 import com.example.android_lesson.R;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Provider;
+import java.security.Security;
 
 public class WalletActivity extends AppCompatActivity {
 
@@ -29,7 +33,7 @@ public class WalletActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wallet);
 
-        MyApplication.setupBouncyCastle();
+        setupBouncyCastle();
 
         findViewById(R.id.btn_create_bip39_wallet).setOnClickListener(view ->
                 ETHWalletHelper.getInstance().generateBip39Wallets()
@@ -118,5 +122,23 @@ public class WalletActivity extends AppCompatActivity {
                 }).start();
             }
         });
+    }
+
+    public static void setupBouncyCastle() {
+        final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
+        if (provider == null) {
+            // Web3j will set up the provider lazily when it's first used.
+            return;
+        }
+        if (provider.getClass().equals(BouncyCastleProvider.class)) {
+            // BC with same package name, shouldn't happen in real life.
+            return;
+        }
+        // Android registers its own BC provider. As it might be outdated and might not include
+        // all needed ciphers, we substitute it with a known BC bundled in the app.
+        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
+        // of that it's possible to have another BC implementation loaded in VM.
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+        Security.insertProviderAt(new BouncyCastleProvider(), 1);
     }
 }
