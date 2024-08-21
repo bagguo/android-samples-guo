2aa752839e9a27dc8e5b9beaf27b8fd8ff2bc955
darren <bagguo23@gmail.com>
2024-08-21 11:06:11 +0800

    create RN context in background

---
+++ react_native_sample/src/main/java/com/example/react_native_sample/App.kt
@@ -0,0 +1,38 @@
+package com.example.react_native_sample
+
+import android.app.Application
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.shell.MainReactPackage
+import com.facebook.soloader.SoLoader
+import java.util.Arrays
+
+
+class App : Application(), ReactApplication {
+
+    private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): List<ReactPackage> {
+            return Arrays.asList<ReactPackage>(
+                MainReactPackage() // Add additional packages here
+            )
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this,  /* native exopackage */false)
+    }
+}
--- react_native_sample/src/main/java/com/example/react_native_sample/MainActivity.kt
+++ react_native_sample/src/main/java/com/example/react_native_sample/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.react_native_sample
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactInstanceManager
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val mReactInstanceManager: ReactInstanceManager by lazy {
+        (application as ReactApplication).reactNativeHost.reactInstanceManager
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mReactInstanceManager.createReactContextInBackground()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mReactInstanceManager.onHostDestroy(this)
     }
 }
