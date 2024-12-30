a88c9d26d8e8d0eda90bad89ec045e89b4042658
darren <bagguo23@gmail.com>
2024-12-30 15:31:19 +0800

    flow: emit() collect() combine()

--- app/src/main/java/com/example/android_lesson/kotlin/flow/FlowSample.kt
+++ app/src/main/java/com/example/android_lesson/kotlin/flow/FlowSample.kt
@@ -5,13 +5,20 @@ import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-class FlowSample {
-
-}
+/**
+ * Flow:
+ * flow()：创建一个Flow实例
+ *
+ * emit()：发射
+ * collect()：收集, emit一个值，collect收到该值
+ *
+ * combine()：组合
+ */
 
 fun simple(): Flow<Int> = flow {
     for (i in 1..3) {
         delay(100)
+        // 发送
         emit(i)
     }
 }
@@ -26,5 +33,22 @@ fun main() = runBlocking<Unit> {
     }
 
 
+    // 收集
     simple().collect { value -> println(value) }
+
+    //combine
+    // 返回一个 Flow，其值由转换函数通过组合每个 flow 最近发出的值生成。
+    //可以用以下示例进行演示：
+    val flow = flowOf(1, 2).onEach {
+        delay(10)
+    }
+    val flow2 = flowOf("a", "b", "c").onEach {
+        delay(15)
+    }
+
+    flow.combine(flow2) { i, s ->
+        i.toString() + s
+    }.collect {
+        println(it) // Will print "1a 2a 2b 2c"
+    }
 }
