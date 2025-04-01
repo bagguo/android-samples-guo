dacbbecc4e2f766621c02cc0baf31f12ff29e4ce
darren <bagguo23@gmail.com>
2025-04-01 18:49:12 +0800

    extend ReactActivity start rn with view, can't use

---
+++ react_native_sample/src/main/java/com/example/react_native_sample/ExtendReactActivity.java
@@ -0,0 +1,46 @@
+package com.example.react_native_sample;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+/**
+ * fixme: can't use
+ * @author Darren.eth
+ * @title 继承ReactActivity承载rn组件
+ */
+public class ExtendReactActivity extends ReactActivity {
+
+    public static void launch(Context context) {
+        context.startActivity(
+                new Intent(context, ExtendReactActivity.class)
+        );
+    }
+
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "MimoWalletRnapp";
+    }
+
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+     * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+                this,
+                getMainComponentName(),
+                // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+                DefaultNewArchitectureEntryPoint.getFabricEnabled());
+    }
+}
--- react_native_sample/src/main/java/com/example/react_native_sample/MainActivity.kt
+++ react_native_sample/src/main/java/com/example/react_native_sample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.react_native_sample
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactInstanceManager
@@ -9,19 +10,23 @@ import com.facebook.react.ReactInstanceManager
 class MainActivity : AppCompatActivity() {
 
 
-    private val mReactInstanceManager: ReactInstanceManager by lazy {
-        (application as ReactApplication).reactNativeHost.reactInstanceManager
-    }
+//    private val mReactInstanceManager: ReactInstanceManager by lazy {
+//        (application as ReactApplication).reactNativeHost.reactInstanceManager
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mReactInstanceManager.createReactContextInBackground()
+//        mReactInstanceManager.createReactContextInBackground()
+
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            ExtendReactActivity.launch(this@MainActivity)
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mReactInstanceManager.onHostDestroy(this)
+//        mReactInstanceManager.onHostDestroy(this)
     }
 }
