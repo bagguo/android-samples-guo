4820c4012ca5733fb63a61cb76a31a1b7be28ca8
darren <bagguo23@gmail.com>
2025-05-29 10:12:36 +0800

    clipboard \ orientation Activity

---
+++ app/src/main/java/com/example/android_lesson/ui/clipboard/ClipboardSampleActivity.kt
@@ -0,0 +1,78 @@
+package com.example.android_lesson.ui.clipboard
+
+import android.content.ClipData
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_lesson.R
+import com.example.android_lesson.databinding.ActivityClipboardSampleBinding
+
+// TODO: 未完成
+class ClipboardSampleActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityClipboardSampleBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        var clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+        var pasteData: String = ""
+
+//        // Gets the ID of the "paste" menu item.
+//        val pasteItem: MenuItem = menu.findItem(R.id.menu_paste)
+//
+//// If the clipboard doesn't contain data, disable the paste menu item.
+//// If it does contain data, decide whether you can handle the data.
+//        pasteItem.isEnabled = when {
+//            !clipboard.hasPrimaryClip() -> {
+//                false
+//            }
+//
+//            !(clipboard.primaryClipDescription?.hasMimeType(MIMETYPE_TEXT_PLAIN)) -> {
+//                // Disables the paste menu item, since the clipboard has data but it
+//                // isn't plain text.
+//                false
+//            }
+//
+//            else -> {
+//                // Enables the paste menu item, since the clipboard contains plain text.
+//                true
+//            }
+//        }
+    }
+
+    /**
+     * 获取剪切板内容
+     */
+    fun getClipboardContent(): String? {
+        // 获取系统剪贴板
+        val clipboard: ClipboardManager =
+            getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        // 返回数据
+        val clipData: ClipData? = clipboard.primaryClip
+        if (clipData == null || clipData.itemCount <= 0) {
+            return ""
+        }
+        val item = clipData.getItemAt(0)
+        /**
+         *清空剪切板
+         */
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            //要api28以上
+            clipboard.clearPrimaryClip()
+        } else {
+            clipboard.setPrimaryClip(ClipData(null))
+        }
+        return if (item == null || item.text == null) {
+            ""
+        } else item.text.toString()
+    }
+}
