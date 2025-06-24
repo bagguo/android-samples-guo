5fa6f110a695e01e7462bee296df94d3ef26a4ef
darren <bagguo23@gmail.com>
2025-06-24 11:50:39 +0800

    enum 序列化

---
+++ app/src/main/java/com/example/guo/language/kotlin/enums/Color.kt
@@ -0,0 +1,32 @@
+package com.example.guo.language.kotlin.enums
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
+
+fun main() {
+    println(User())
+}
+
+data class User(
+    var userType: UserType = UserType.ADMIN,
+)
+
+enum class UserType(
+    // 指定typeValue序列化
+    @get:JsonValue
+    val typeValue: Int,
+) {
+    ADMIN(2),
+    USER(4),
+    ;
+
+    companion object {
+        // 指定creator反序列化
+        @JsonCreator
+        @JvmStatic
+        fun creator(typeValue: Int): UserType {
+            val found = UserType.values().find { it.typeValue == typeValue }
+            return found ?: throw IllegalArgumentException("not found IconEnum with $typeValue")
+        }
+    }
+}
--- app/src/main/java/com/example/guo/widgets/recyclerview/expose/ExposeRecyclerView.kt
+++ app/src/main/java/com/example/guo/widgets/recyclerview/expose/ExposeRecyclerView.kt
@@ -10,75 +10,95 @@ import androidx.recyclerview.widget.RecyclerView
 /**
  * 曝光埋点
  */
-class ExposeRecyclerView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
-) : RecyclerView(//自定义RecyclerView
-    context, attrs, defStyle
-) {
+class ExposeRecyclerView
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyle: Int = 0,
+    ) : RecyclerView( // 自定义RecyclerView
+            context,
+            attrs,
+            defStyle,
+        ) {
 
-    interface ItemViewVisibleListener {//回调接口（显示、隐藏）
-    fun onItemViewVisible(position: Int)
-        fun onItemViewGone(position: Int)
-    }
+        interface ItemViewVisibleListener {
+            // 回调接口（显示、隐藏）
+            fun onItemViewVisible(position: Int)
 
-    // 回调监听器
-    var itemViewVisibleListener: ItemViewVisibleListener? = null
+            fun onItemViewGone(position: Int)
+        }
 
-    init {
-        //注册滚动监听
-        addOnScrollListener(object : OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                handleScrolled()
-            }
-        })
-    }
+        // 回调监听器
+        var itemViewVisibleListener: ItemViewVisibleListener? = null
 
-    //处理滚动回调，第一次加载数据也会触发
-    private fun handleScrolled() {
-        when (layoutManager) {
-            is LinearLayoutManager -> {
+        init {
+            // 注册滚动监听
+            addOnScrollListener(
+                object : OnScrollListener() {
+                    override fun onScrolled(
+                        recyclerView: RecyclerView,
+                        dx: Int,
+                        dy: Int,
+                    ) {
+                        super.onScrolled(recyclerView, dx, dy)
+                        handleScrolled()
+                    }
+                },
+            )
+        }
+
+        // 处理滚动回调，第一次加载数据也会触发
+        private fun handleScrolled() {
+            when (layoutManager) {
+                is LinearLayoutManager -> {
 //                handleVisibleItem(
 //                    recyclerView,
 //                    (layoutManager as LinearLayoutManager).findFirstVisibleItemPosition(),
 //                    (layoutManager as LinearLayoutManager).findLastVisibleItemPosition(),
 //                    (layoutManager as LinearLayoutManager).itemCount
 //                )
-            }
-        }
-    }
-
-    private fun handleVisibleItem(
-        recyclerView: RecyclerView, firstVisiblePosition: Int, lastVisiblePosition: Int, itemCount: Int
-    ) {
-        for (i in 0 until itemCount) {//1
-            when (i) {
-                in firstVisiblePosition..lastVisiblePosition -> {
-                    callListenerByItemViewRect(recyclerView.findViewHolderForAdapterPosition(i))
-                }
-                else -> {
-                    itemViewVisibleListener?.onItemViewGone(i)
                 }
             }
         }
-    }
 
-    //回调不同的接口
-    private fun callListenerByItemViewRect(viewHolder: ViewHolder?) {
-        viewHolder?.apply {
-            if (itemView.isExpose()) {
-                itemViewVisibleListener?.onItemViewVisible(adapterPosition)
-            } else {
-                itemViewVisibleListener?.onItemViewGone(adapterPosition)
+        private fun handleVisibleItem(
+            recyclerView: RecyclerView,
+            firstVisiblePosition: Int,
+            lastVisiblePosition: Int,
+            itemCount: Int,
+        ) {
+            for (i in 0 until itemCount) { // 1
+                when (i) {
+                    in firstVisiblePosition..lastVisiblePosition -> {
+                        callListenerByItemViewRect(recyclerView.findViewHolderForAdapterPosition(i))
+                    }
+
+                    else -> {
+                        itemViewVisibleListener?.onItemViewGone(i)
+                    }
+                }
             }
         }
-    }
 
-    //判断曝光面积是否满足条件
-    fun View.isExpose(): Boolean {//2
-        val rect = Rect()
-        val isVisibleRect: Boolean = this.getGlobalVisibleRect(rect)
-        return (isVisibleRect
-                && rect.height() * rect.width() >= measuredHeight * measuredWidth * 2 / 3)
+        // 回调不同的接口
+        private fun callListenerByItemViewRect(viewHolder: ViewHolder?) {
+            viewHolder?.apply {
+                if (itemView.isExpose()) {
+                    itemViewVisibleListener?.onItemViewVisible(adapterPosition)
+                } else {
+                    itemViewVisibleListener?.onItemViewGone(adapterPosition)
+                }
+            }
+        }
+
+        // 判断曝光面积是否满足条件
+        fun View.isExpose(): Boolean { // 2
+            val rect = Rect()
+            val isVisibleRect: Boolean = this.getGlobalVisibleRect(rect)
+            return (
+                isVisibleRect &&
+                    rect.height() * rect.width() >= measuredHeight * measuredWidth * 2 / 3
+            )
+        }
     }
-}
