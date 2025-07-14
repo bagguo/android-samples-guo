7a807acb372f3198928b598fecff28a66947cd03
darren <bagguo23@gmail.com>
2025-07-14 14:49:20 +0800

    ViewPager2Adapter

---
+++ app/src/main/java/com/example/guo/adapter/ViewPager2Adapter.kt
@@ -0,0 +1,17 @@
+package com.example.guo.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPager2Adapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle,
+    private val fragments: List<Fragment>
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int = fragments.size
+
+    override fun createFragment(position: Int): Fragment = fragments[position]
+}
--- app/src/main/java/com/example/guo/ui/fragment/bottomsheet/TabVP2BottomSheetDialogFragment.kt
+++ app/src/main/java/com/example/guo/ui/fragment/bottomsheet/TabVP2BottomSheetDialogFragment.kt
@@ -5,9 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.example.guo.adapter.ViewPager2Adapter
 import com.example.guo.databinding.FragmentBottomSheetTabVp2Binding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.tabs.TabLayoutMediator
@@ -17,29 +16,28 @@ import com.google.android.material.tabs.TabLayoutMediator
  * @author Darren.eth
  */
 class TabVP2BottomSheetDialogFragment : BottomSheetDialogFragment() {
-
     companion object {
         private const val TAB_NUM = 4
 
-        fun newInstance(): TabVP2BottomSheetDialogFragment {
-            return TabVP2BottomSheetDialogFragment()
-        }
+        fun newInstance(): TabVP2BottomSheetDialogFragment = TabVP2BottomSheetDialogFragment()
     }
 
     private lateinit var binding: FragmentBottomSheetTabVp2Binding
     private val fragments = ArrayList<Fragment>()
 
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         binding = FragmentBottomSheetTabVp2Binding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
         super.onViewCreated(view, savedInstanceState)
         binding.initTab()
     }
@@ -50,7 +48,7 @@ class TabVP2BottomSheetDialogFragment : BottomSheetDialogFragment() {
             tab.addTab(binding.tab.newTab().setText("tab$i"))
         }
 
-        val adapter = ChildFragmentsAdapter(requireActivity(), fragments)
+        val adapter = ViewPager2Adapter(childFragmentManager, lifecycle, fragments)
         vp2.adapter = adapter
 
         TabLayoutMediator(tab, vp2) { tab, position ->
@@ -65,19 +63,4 @@ class TabVP2BottomSheetDialogFragment : BottomSheetDialogFragment() {
             }
         vp2.registerOnPageChangeCallback(onPageChangeCallback)
     }
-
-    class ChildFragmentsAdapter(
-        fragmentActivity: FragmentActivity,
-        private val fragments: List<Fragment>
-    ) : FragmentStateAdapter(fragmentActivity) {
-
-        override fun getItemCount(): Int {
-            return fragments.size
-        }
-
-        override fun createFragment(position: Int): Fragment {
-            return fragments[position]
-        }
-    }
-
 }
