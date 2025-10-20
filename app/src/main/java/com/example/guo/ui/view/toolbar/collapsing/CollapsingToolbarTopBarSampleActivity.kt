538e90a4fe166969e7615046a375ea0dcfcd7402
darren <bagguo23@gmail.com>
2025-10-20 19:36:09 +0800

    ConsecutiveScrollerSampleActivity

---
+++ app/src/main/java/com/example/guo/ui/view/toolbar/collapsing/CollapsingToolbarTopBarSampleActivity.kt
@@ -0,0 +1,15 @@
+package com.example.guo.ui.view.toolbar.collapsing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.guo.R
+
+/**
+ * CollapsingToolbar实现吸顶效果
+ */
+class CollapsingToolbarTopBarSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collapsing_toolbar_top_bar_sample)
+    }
+}
